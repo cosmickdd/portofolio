b32f7e04145c7964b3380978bf09cfff8bc0348e
utkarsh tiwari <utkarsh2313003@akgec.ac.in>
2024-10-15 21:12:12 +0530

    jj

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,12 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Utkarsh</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+          As a craftsman of code,<br className='sm:block hidden' />
+          I do shape the web,  
+          With artful designs and strings I deftly thread.
           </p>
         </div>
       </div>
